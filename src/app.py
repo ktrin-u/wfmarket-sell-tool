661db3fe9ee25e1285ffc6fe0441090255d8751f
ktrin-u <kdtrinidad2@up.edu.ph>
2025-01-09 21:19:53 +0800

    wip: add concurrency

--- src/app.py
+++ src/app.py
@@ -1,135 +1,174 @@
 """
 Basic program that generates bottom N order prices of the specified items
 """
-from pprint import pprint
-from typing import TypeVar
-from enum import auto, StrEnum
-import json
-import requests
 import warnings
-import time
+import asyncio
+import aiohttp
+import logging
+from required_types import T, Order, Status, WFMarketResponse
 
-
-T = TypeVar("T")
-K = TypeVar("K")
-warnings.simplefilter("always", RuntimeWarning)  # always show RuntimeWarnings
 
 ENDPOINT = "https://api.warframe.market/v1"  # warframe.market api endpoint
-TIMEOUT = 3  # https request timeout
-REQUEST_LIMIT:int = 3  # wfmarket ToS states a max of 3 requests per second
-request_counter:int = 0  # keeps track of number of requests made during the current second
-last_second_tracker: time.struct_time = time.localtime()  # to ensure compliance with ToS of 3 requests per second
+TIMEOUT: int = 3  # https request timeout
+REQUEST_LIMIT: int = 3  # wfmarket ToS states a max of 3 requests per second
+request_counter: int = 0  # keeps track of number of requests made during the current second
+
+logging.basicConfig(filename="app.log", level=logging.INFO)
+warnings.simplefilter("always", RuntimeWarning)  # always show RuntimeWarnings
 
 
-class Status(StrEnum):
-    INGAME = auto()  # type: ignore
-    ONLINE = auto()  # type: ignore
-    OFFLINE = auto()  # type: ignore
-
-def check_request_time_valid() -> bool:
-    global REQUEST_LIMIT, request_counter, last_request_timestamp
-    request_time = time.localtime()
-
-    if request_time.tm_sec != last_second_tracker:
-        request_counter = 0  # reset request_counter when a second has elapsed
-
-    if request_counter >= REQUEST_LIMIT:
-        return False
-
-def get_item_prices(item_name: str) -> list[dict[str, object]]:
-    """Get the order details of item_name as a json"""
-    if LAST_REQUEST_TIMESTAMP - request_time >:
-    with requests.get(
-        url=f"{ENDPOINT}/items/{item_name.lower()}/orders",
-        timeout=TIMEOUT
-    ) as result:
-        match result.status_code:
-            case 200:
-                payload = json.loads(result.text).get("payload")
-                if type(payload) is dict:
-                    orders = payload.get("orders")  # type: ignore
-                    if orders is None:
-                        raise Exception("no orders found")
-                    return orders  # type: ignore
-
-                raise Exception("unexpected payload received from json loads")
-
-            case _:
-                raise ValueError(f"expected http status 200, got http status {result.status_code}")
-
-
-def filter_sell_orders(orders: list[dict[str, T]]) -> list[dict[str, T]]:
+class WFMarketTool:
     """
-    Removes all non-sell orders in the order list
+    Class for the WF Market sell tool
     """
-    if type(orders) is not list:
-        raise TypeError("argument is expected to be of type list")
+    def __init__(self):
+        self._session = aiohttp.ClientSession()
+        self._lock = asyncio.Lock()
+        self._logger = logging.getLogger(__name__)
 
-    def filter_func(order: dict[str, T]) -> bool:
-        order_type = order.get("order_type")
-        if order_type is not None:
-            match order_type:
-                case "sell":
-                    return remove_non_in_game(order)
-                case "buy":
-                    return False
+        self._request_timer = asyncio.create_task(self._request_timer_update())
+
+    def __del__(self):
+        self._request_timer.cancel()
+
+    async def _request_timer_update(self) -> None:
+        global request_counter
+        while True:
+            async with self._lock:
+                request_counter = 0
+                self._logger.info('request counter refreshed')
+            await asyncio.sleep(1)
+
+    async def check_request_time_valid(self) -> bool:
+        global REQUEST_LIMIT, request_counter
+
+        async with self._lock:  # need to lock this part to ensure that request_counter is consistent
+            if request_counter >= REQUEST_LIMIT:
+                return False
+
+            request_counter += 1
+            self._logger.info("new request made")
+            return True
+
+    async def get_item_orders(self, item_name: str) -> WFMarketResponse:
+        self._logger.info(f"attempting to acquire {item_name}")
+        if not (await self.check_request_time_valid()):
+            self._logger.info("request_limit reached, trying again in 0.5s")
+            await asyncio.sleep(0.5)
+            return await self.get_item_orders(item_name)
+
+        async with self._session.get(
+            url=f"{ENDPOINT}/items/{item_name.lower()}/orders",
+        ) as result:
+            match result.status:
+                case 200:
+                    logging.info(f"successfully acquired {item_name}")
+                    return await result.json()
                 case _:
-                    warnings.warn(f"unsupported order type \"{order_type}\" found")
-                    return False
-        warnings.warn(f"order with id {order["id"]} is invalid", RuntimeWarning, )
-        return False
+                    logging.info(f"expected http status 200, got http code {result.status}")
+                    logging.info(f"failed to acquire {item_name}")
+                    warnings.warn(f"unable to acquire {item_name}")
+                    return {}
 
-    def remove_non_in_game(order: dict[str, T]) -> bool:
-        user = order.get("user")
-        if user is not None:
-            status = user.get("status")  # type: ignore
-            if status is not None:
-                match status:
-                    case Status.INGAME.value:
-                        return True
-                    case Status.ONLINE.value:
+    async def get_item_prices(self, item_name: str) -> list[Order]:
+        """Get the order details of item_name as a json"""
+        result = await self.get_item_orders(item_name)
+        payload = result.get("payload")
+        if payload is not None:
+            orders: list[Order] = payload.get("orders")
+            return orders
+
+        warnings.warn("unable to get orders from payload None")
+        return list()
+
+    async def filter_sell_orders(self, orders: list[Order]) -> list[Order]:
+        """
+        Removes all non-sell orders in the order list
+        """
+        if type(orders) is not list:
+            raise TypeError("argument is expected to be of type list")
+
+        def filter_func(order: Order) -> bool:
+            order_type = order.get("order_type")
+            if order_type is not None:
+                match order_type:
+                    case "sell":
+                        return remove_non_in_game(order)
+                    case "buy":
                         return False
-                    case Status.OFFLINE.value:
+                    case _:
+                        warnings.warn(f"unsupported order type \"{order_type}\" found")
                         return False
-                    case _:  # type: ignore
-                        warnings.warn(f"unsupported status type found in order id {order["id"]}")
-                        return False
-        raise Exception(f"order {order["id"]} has no user key")
+            warnings.warn(f"order with id {order["id"]} is invalid", RuntimeWarning, )
+            return False
 
-    return list(filter(filter_func, orders))
+        def remove_non_in_game(order: Order) -> bool:
+            user = order.get("user")
+            if user is not None:
+                status = user.get("status")  # type: ignore
+                if status is not None:
+                    match status:
+                        case Status.INGAME.value:
+                            return True
+                        case Status.ONLINE.value:
+                            return False
+                        case Status.OFFLINE.value:
+                            return False
+                        case _:  # type: ignore
+                            warnings.warn(f"unsupported status type found in order id {order["id"]}")
+                            return False
+            raise Exception(f"order {order["id"]} has no user key")
 
+        return list(filter(filter_func, orders))
 
-def get_plat_prices(sell_orders: list[dict[str, T]], sort_descending: bool = False) -> list[int]:
-    """
-    Gets all the platinum prices from the a list of sell orders
-    """
-    if type(sell_orders) is not list:
-        raise TypeError("argument is expected to be of type list")
+    async def get_plat_prices(self, sell_orders: list[Order], sort_descending: bool = False) -> list[int]:
+        """
+        Gets all the platinum prices from the a list of sell orders
+        """
+        if type(sell_orders) is not list:
+            raise TypeError("argument is expected to be of type list")
 
-    prices: list[int] = []
-    for order in sell_orders:
-        plat = order.get("platinum")
-        if type(plat) is int:
-            prices.append(plat)
-            continue
-        warnings.warn(f"order {order["id"]} has no platinum key", RuntimeWarning)
+        prices: list[int] = []
+        for order in sell_orders:
+            plat = order.get("platinum")
+            if type(plat) is int:
+                prices.append(plat)
+                continue
+            warnings.warn(f"order {order["id"]} has no platinum key", RuntimeWarning)
 
-    if sort_descending:
-        prices.sort(reverse=True)
+        if sort_descending:
+            prices.sort(reverse=True)
+            return prices
+
+        prices.sort()
         return prices
 
-    prices.sort()
-    return prices
+    async def get_floor_prices(self, item_name: str, order_count: int = 5) -> str:
+        resulting_orders = await self.get_item_prices(item_name)
+        sell_orders = await self.filter_sell_orders(resulting_orders)
+        plat_prices = await self.get_plat_prices(sell_orders)
+        ret = f"{item_name} bottom {order_count} floor prices are: {plat_prices[:order_count]}"
+        print(ret)
+        return ret
+
+    async def close(self):
+        await self._session.close()
 
 
-def get_floor_prices(item_name: str, order_count: int = 5) -> str:
-    resulting_orders = get_item_prices(item_name)
-    sell_orders = filter_sell_orders(resulting_orders)
-    plat_prices = get_plat_prices(sell_orders)
-    return f"{item_name} bottom {order_count} floor prices are: {plat_prices[:order_count]}"
+async def main():
+    # items = ["overextended", "narrow_minded", "catalyzing_shields", "blind_rage"]
+    tool = WFMarketTool()
 
+    # for item in items:
+    #     query = asyncio.create_task(tool.get_floor_prices(item))
+    queries = asyncio.gather(
+        tool.get_floor_prices("overextended"),
+        tool.get_floor_prices("narrow_minded"),
+        tool.get_floor_prices("catalyzing_shields"),
+        tool.get_floor_prices("blind_rage"),
+    )
+
+    # await tool.close()
 
 if __name__ == "__main__":
-    for item in ["overextended", "narrow_minded", "catalyzing_shields", "blind_rage"]:
-        pprint(get_floor_prices(item))
-        time.sleep(1)
+    main_task = asyncio.run(main())
---
+++ src/required_types.py
@@ -0,0 +1,53 @@
+"""
+Collection of Types and Protocols to ensure static typing
+"""
+from typing import TypeVar, TypedDict, Required
+from enum import StrEnum, auto
+
+
+T = TypeVar("T")
+K = TypeVar("K")
+
+
+class Status(StrEnum):
+    INGAME = auto()
+    ONLINE = auto()
+    OFFLINE = auto()
+
+
+class OrderType(StrEnum):
+    BUY = auto()
+    SELL = auto()
+
+
+class User(TypedDict):
+    reputation: int
+    locale: str
+    avatar: str
+    ingame_name: str
+    last_seen: str
+    id: str
+    region: str
+    status: Status
+
+
+class Order(TypedDict, total=False):
+    creation_date: str
+    visible: bool
+    quantity: int
+    user: User
+    last_update: str
+    platinum: int
+    order_type: OrderType
+    platform: str
+    id: Required[str]
+    mod_rank: str
+    region: str
+
+
+class Payload(TypedDict):
+    orders: list[Order]
+
+
+class WFMarketResponse(TypedDict, total=False):
+    payload: Payload
